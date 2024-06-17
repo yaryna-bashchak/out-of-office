b242f71f9bf061fe63977e936b1ac4ad6344d74b
yaryna-bashchak <yaryna.bashchak@gmail.com>
2024-06-17 14:25:11 +0300

    implement LeaveRequestContext

---
+++ outofoffice.client/src/app/context/LeaveRequestContext.tsx
@@ -0,0 +1,76 @@
+import { createContext, useState, useEffect, ReactNode } from 'react';
+import agent from '../api/agent';
+import { AbsenceReason, LeaveRequest, LeaveRequestPayload, LeaveRequestStatus, RequestType } from '../models/leaveRequest';
+
+interface LeaveRequestContextType {
+  leaveRequests: LeaveRequest[];
+  statuses: LeaveRequestStatus[];
+  types: RequestType[];
+  absenceReasons: AbsenceReason[];
+  addLeaveRequest: (leaveRequest: LeaveRequestPayload) => Promise<void>;
+  editLeaveRequestInfo: (id: number, leaveRequest: LeaveRequestPayload) => Promise<void>;
+  editLeaveRequestStatus: (id: number, statusId: number,) => Promise<void>;
+}
+
+const LeaveRequestContext = createContext<LeaveRequestContextType | undefined>(undefined);
+
+interface LeaveRequestProviderProps {
+  children: ReactNode;
+}
+
+export const LeaveRequestProvider = ({ children }: LeaveRequestProviderProps) => {
+  const [leaveRequests, setLeaveRequests] = useState<LeaveRequest[]>([]);
+  const [statuses, setStatuses] = useState<LeaveRequestStatus[]>([]);
+  const [types, setTypes] = useState<RequestType[]>([]);
+  const [absenceReasons, setAbsenceReasons] = useState<AbsenceReason[]>([]);
+
+  useEffect(() => {
+    const loadLeaveRequests = async () => {
+      const leaveRequests = await agent.LeaveRequest.getAll();
+      setLeaveRequests(leaveRequests);
+    };
+    loadLeaveRequests();
+
+    const loadStatuses = async () => {
+      const statuses = await agent.LeaveRequest.getStatuses();
+      setStatuses(statuses);
+    };
+    loadStatuses();
+
+    const loadTypes = async () => {
+      const types = await agent.LeaveRequest.getTypes();
+      setTypes(types);
+    };
+    loadTypes();
+
+    const loadAbsenceReasons = async () => {
+      const absenceReasons = await agent.LeaveRequest.getAbsenceReasons();
+      setAbsenceReasons(absenceReasons);
+    };
+    loadAbsenceReasons();
+  }, []);
+
+  const addLeaveRequest = async (leaveRequest: LeaveRequestPayload) => {
+    const newLeaveRequest = await agent.LeaveRequest.add(leaveRequest);
+    setLeaveRequests([...leaveRequests, newLeaveRequest]);
+  };
+
+  const editLeaveRequestInfo = async (id: number, leaveRequest: LeaveRequestPayload) => {
+    const updatedLeaveRequest = await agent.LeaveRequest.updateInfo(id, leaveRequest);
+    setLeaveRequests(leaveRequests.map(emp => (emp.id === id ? updatedLeaveRequest : emp)));
+  };
+
+  const editLeaveRequestStatus = async (id: number, statusId: number) => {
+    const updatedLeaveRequest = await agent.LeaveRequest.updateStatus(id, statusId);
+    setLeaveRequests(leaveRequests.map(emp => (emp.id === id ? updatedLeaveRequest : emp)));
+  };
+
+  return (
+    <LeaveRequestContext.Provider value={{ leaveRequests, types, statuses, absenceReasons, addLeaveRequest, editLeaveRequestInfo, editLeaveRequestStatus }}>
+      {children}
+    </LeaveRequestContext.Provider>
+  );
+};
+
+export default LeaveRequestContext;
+
