4d7626e55f9fa6bd9ed4a3068e27f41809cd9a87
yaryna-bashchak <yaryna.bashchak@gmail.com>
2024-06-18 13:08:35 +0300

    fix bag: update hr and project manager lists when employee list updates

--- outofoffice.client/src/app/context/EmployeeContext.tsx
+++ outofoffice.client/src/app/context/EmployeeContext.tsx
@@ -68,11 +68,30 @@ export const EmployeeProvider = ({ children }: EmployeeProviderProps) => {
   const addEmployee = async (employee: EmployeePayload) => {
     const newEmployee = await agent.Employee.add(employee);
     setEmployees([...employees, newEmployee]);
+
+    if (newEmployee.position.name === 'HR Manager') {
+      setHrManagers([...hrManagers, newEmployee]);
+    }
+    if (newEmployee.position.name === 'Project Manager') {
+      setProjectManagers([...projectManagers, newEmployee]);
+    }
   };
 
   const editEmployee = async (id: number, employee: EmployeePayload) => {
     const updatedEmployee = await agent.Employee.update(id, employee);
     setEmployees(employees.map(emp => (emp.id === id ? updatedEmployee : emp)));
+
+    if (updatedEmployee.position.name === 'HR Manager') {
+      setHrManagers(hrManagers.map(emp => (emp.id === id ? updatedEmployee : emp)));
+    } else {
+      setHrManagers(hrManagers.filter(emp => emp.id !== id));
+    }
+
+    if (updatedEmployee.position.name === 'Project Manager') {
+      setProjectManagers(projectManagers.map(emp => (emp.id === id ? updatedEmployee : emp)));
+    } else {
+      setProjectManagers(projectManagers.filter(emp => emp.id !== id));
+    }
   };
 
   return (
